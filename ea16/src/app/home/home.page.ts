2e2aaff7e83b9f06066a8f2549c03b3907e68f7d
Michellee <22005@portalcedi.edu.mx>
2025-03-13 08:53:00 -0600

    tareitas

--- ea16/src/app/home/home.page.ts
+++ ea16/src/app/home/home.page.ts
@@ -1,53 +1,51 @@
-import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 import { CommonModule } from '@angular/common';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
+import { TaskService, Task } from '../task.service'; 
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-home',
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule, ReactiveFormsModule],
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  imports: [IonicModule, CommonModule, FormsModule],
 })
-export class HomePage {
-  taskForm: FormGroup;
-  tasks: Array<{ title: string; dueDate: string; done: boolean }> = [];
-  filter: string = 'all';
+export class HomePage implements OnInit {
+  taskText = ''; 
+  tasks$: Observable<Task[]> = new Observable(); // Cambiar a Observable para Firestore
+  editingTaskId: string | null = null; 
 
-  constructor(private fb: FormBuilder) {
-    this.taskForm = this.fb.group({
-      title: ['', [Validators.required, Validators.minLength(3)]],
-      dueDate: ['', Validators.required]
-    });
+  constructor(private taskService: TaskService) {} 
+
+  ngOnInit() {
+    this.tasks$ = this.taskService.getTasks(); // Obtener tareas desde Firestore
   }
 
   addTask() {
-    if (this.taskForm.valid) {
-      this.tasks.push({
-        title: this.taskForm.value.title,
-        dueDate: this.taskForm.value.dueDate,
-        done: false
-      });
-      this.taskForm.reset();
+    if (this.taskText.trim()) {
+      const newTask: Task = { title: this.taskText, done: false };
+
+      if (this.editingTaskId) {
+        this.taskService.updateTask(this.editingTaskId, { title: this.taskText }).then(() => {
+          this.editingTaskId = null; 
+          this.taskText = '';
+        });
+      } else {
+        this.taskService.addTask(newTask).then(() => {
+          this.taskText = '';
+        });
+      }
     }
   }
 
-  toggleCompletion(index: number) {
-    this.tasks[index].done = !this.tasks[index].done;
+  editTask(task: Task) {
+    this.taskText = task.title;
+    this.editingTaskId = task.id || null;
   }
 
-  deleteTask(index: number) {
-    this.tasks.splice(index, 1);
-  }
-
-  getFilteredTasks() {
-    if (this.filter === 'completed') {
-      return this.tasks.filter(task => task.done);
-    } else if (this.filter === 'pending') {
-      return this.tasks.filter(task => !task.done);
-    }
-    return this.tasks;
+  deleteTask(taskId: string) {
+    this.taskService.deleteTask(taskId);
   }
 }
--- ea16/src/environments/environment.ts
+++ ea16/src/environments/environment.ts
@@ -1,17 +1,18 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
+export const environment = {
+  production: false,
+  firebaseConfig: {
   apiKey: "AIzaSyDDpiOv-Fx26ClOMy4B_ul0RuY2sJvBeVI",
   authDomain: "ea16-7e956.firebaseapp.com",
   projectId: "ea16-7e956",
   storageBucket: "ea16-7e956.firebasestorage.app",
   messagingSenderId: "338480204912",
   appId: "1:338480204912:web:ec7a604b1193375d4bb8b5"
+}
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
